7d883ab0785d0b39c96153b3ad3c3ce1c985eea4
CarlosMunozDiaz <carlos10md@gmail.com>
2022-09-11 18:33:35 +0200

    Nuevos estilos y disposición de elementos HTML

---
+++ js/main.js
@@ -0,0 +1,94 @@
+console.log("Funciona el script");
+
+//INIT FUNCTIONS
+init2a();
+init2b();
+init16_18();
+init35();
+init10();
+init42a();
+init42b();
+init48a();
+init48b();
+init48c();
+
+//FUNCTIONS
+function init2a() {
+    let url = '';
+    d3.csv('', function(data, error) {
+        if (error) throw error;
+
+    });
+}
+
+function init2b() {
+    let url = '';
+    d3.csv('', function(data, error) {
+        if (error) throw error;
+        
+    });
+}
+
+function init16_18() {
+    let url = '';
+    d3.csv('', function(data, error) {
+        if (error) throw error;
+        
+    });
+}
+
+function init35() {
+    let url = '';
+    d3.csv('', function(data, error) {
+        if (error) throw error;
+        
+    });
+}
+
+function init10() {
+    let url = '';
+    d3.csv('', function(data, error) {
+        if (error) throw error;
+        
+    });
+}
+
+function init42a() {
+    let url = '';
+    d3.csv('', function(data, error) {
+        if (error) throw error;
+        
+    });
+}
+
+function init42b() {
+    let url = '';
+    d3.csv('', function(data, error) {
+        if (error) throw error;
+        
+    });
+}
+
+function init48a() {
+    let url = '';
+    d3.csv('', function(data, error) {
+        if (error) throw error;
+        
+    });
+}
+
+function init48b() {
+    let url = '';
+    d3.csv('', function(data, error) {
+        if (error) throw error;
+        
+    });
+}
+
+function init48c() {
+    let url = '';
+    d3.csv('', function(data, error) {
+        if (error) throw error;
+        
+    });
+}
