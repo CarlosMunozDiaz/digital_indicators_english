cf030ed36cd45d54de46e7203583ae672520e346
CarlosMunozDiaz <carlos10md@gmail.com>
2022-09-12 10:26:38 +0200

    Error JS

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ init48c();
 function init2a() {
     let url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRpmJKRvDm4iWZrbYtr2eFi0uQYcV3czLLDugi7M5V3slFP8PJDPHDKyK1Rql6lPUQVMO0AZ8zRk5H6/pub?gid=1296035760&single=true&output=csv';
     let selectElement = document.getElementById('select_2a');
-    d3.csv(url, function(data, error) {
+    d3.csv(url, function(error, data) {
         if (error) throw error;
         console.log(data);
     });
@@ -26,26 +26,26 @@ function init2b() {
     let selectBtns = selectBtnBlock.getElementsByClassName('btn');
     let currentBtn = 'Both';
 
-    d3.csv(url, function(data, error) {
+    d3.csv(url, function(error, data) {
         if (error) throw error;
-        
+        console.log(data);
     });
 }
 
 function init16_18() {
     let url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRpmJKRvDm4iWZrbYtr2eFi0uQYcV3czLLDugi7M5V3slFP8PJDPHDKyK1Rql6lPUQVMO0AZ8zRk5H6/pub?gid=0&single=true&output=csv';
     let selectElement = document.getElementById('select_16_18');
-    d3.csv(url, function(data, error) {
+    d3.csv(url, function(error, data) {
         if (error) throw error;
-        
+        console.log(data);
     });
 }
 
 function init35() {
     let url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRpmJKRvDm4iWZrbYtr2eFi0uQYcV3czLLDugi7M5V3slFP8PJDPHDKyK1Rql6lPUQVMO0AZ8zRk5H6/pub?gid=1437038791&single=true&output=csv';
-    d3.csv(url, function(data, error) {
+    d3.csv(url, function(error, data) {
         if (error) throw error;
-        
+        console.log(data);
     });
 }
 
@@ -54,43 +54,43 @@ function init10() {
     let selectBtnBlock = document.getElementById('buttons_10');
     let selectBtns = selectBtnBlock.getElementsByClassName('btn');
     let currentBtn = 'STEM';
-    d3.csv(url, function(data, error) {
+    d3.csv(url, function(error, data) {
         if (error) throw error;
-        
+        console.log(data);
     });
 }
 
 function init42a() {
     let url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRpmJKRvDm4iWZrbYtr2eFi0uQYcV3czLLDugi7M5V3slFP8PJDPHDKyK1Rql6lPUQVMO0AZ8zRk5H6/pub?gid=1639180365&single=true&output=csv';
-    d3.csv(url, function(data, error) {
+    d3.csv(url, function(error, data) {
         if (error) throw error;
-        
+        console.log(data);
     });
 }
 
 function init42b() {
     let url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRpmJKRvDm4iWZrbYtr2eFi0uQYcV3czLLDugi7M5V3slFP8PJDPHDKyK1Rql6lPUQVMO0AZ8zRk5H6/pub?gid=846839345&single=true&output=csv';
     let selectElement = document.getElementById('select_42b');
-    d3.csv('', function(data, error) {
+    d3.csv(url, function(error, data) {
         if (error) throw error;
-        
+        console.log(data);
     });
 }
 
 function init48a() {
     let url = '';
     let selectElement = document.getElementById('select_48a');
-    d3.csv('', function(data, error) {
+    d3.csv(url, function(error, data) {
         if (error) throw error;
-        
+        console.log(data);
     });
 }
 
 function init48b() {
     let url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRpmJKRvDm4iWZrbYtr2eFi0uQYcV3czLLDugi7M5V3slFP8PJDPHDKyK1Rql6lPUQVMO0AZ8zRk5H6/pub?gid=404906190&single=true&output=csv';
-    d3.csv(url, function(data, error) {
+    d3.csv(url, function(error, data) {
         if (error) throw error;
-        
+        console.log(data);
     });
 }
 
@@ -98,8 +98,9 @@ function init48b() {
 function init48c() {
     let url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRpmJKRvDm4iWZrbYtr2eFi0uQYcV3czLLDugi7M5V3slFP8PJDPHDKyK1Rql6lPUQVMO0AZ8zRk5H6/pub?gid=1192825969&single=true&output=csv';
     let selectElement = document.getElementById('select_48c');
-    d3.csv(url, function(data, error) {
+    d3.csv(url, function(error, data) {
         if (error) throw error;
+        console.log(data);
 
         //Listener
         selectElement.addEventListener('click', function(e) {
