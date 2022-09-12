63e68684d19f48b2d248441d7fc5ea42b0ecd5b7
CarlosMunozDiaz <carlos10md@gmail.com>
2022-09-12 17:12:27 +0200

    Lógica 48b + Estilos

--- js/main.js
+++ js/main.js
@@ -34,6 +34,10 @@ function init2a() {
             alert("Chart 2a > " + e.target.options[e.target.selectedIndex].value);
             //updateChart(e.target.options[e.target.selectedIndex].value)
         });
+
+        //Init elements
+
+
     });
 }
 
@@ -157,10 +161,111 @@ function init48a() {
 }
 
 function init48b() {
+    //Variables
+    let chartBlock = d3.select('#v_fig48b'), chart, x_pre, x_final, y_pre, y_final, line, paths;
+    let width, height, margin = {top: 10, right: 5, bottom: 15, left: 25};
     let url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRpmJKRvDm4iWZrbYtr2eFi0uQYcV3czLLDugi7M5V3slFP8PJDPHDKyK1Rql6lPUQVMO0AZ8zRk5H6/pub?gid=404906190&single=true&output=csv';
     d3.csv(url, function(error, data) {
         if (error) throw error;
-        console.log("48b", data);
+        
+        //DATA
+        let innerData = data.map(function(d) {
+            return {
+                year: d.Year,
+                'Brazil': +d['Brazil'],
+                'Argentina': +d['Argentina'],
+                'Chile': +d['Chile'],
+                'Colombia': +d['Colombia'],
+                'Mexico': +d['Mexico']
+            }
+        });
+        let keys = data.columns.slice(1);
+        let nestedData = keys.map(function(item) {
+            let aux = [];
+            innerData.map(function(d) {
+                aux.push({'year':d.Year, 'value': d[item]});
+            });
+            return {'key': item, 'data': aux}; 
+        });
+        console.log("48b", nestedData);
+
+        //INIT VISUALIZATIONS VARIABLES
+        width = parseInt(chartBlock.style('width')) - margin.left - margin.right,
+        height = parseInt(chartBlock.style('height')) - margin.top - margin.bottom;
+
+        chart = chartBlock
+            .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
+
+        //X Axis
+        x_pre = d3.scaleBand()
+            .domain(nestedData[0].data.map(function(d) { return d.Year; }))
+            .range([0, width]);
+        
+        x_final = function(g){
+            g.call(d3.axisBottom(x_pre))
+            g.call(function(g){g.selectAll('.tick line').remove();})
+            g.call(function(g){g.select('.domain').remove()});
+        }
+
+        chart.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .attr('class','x_axis')
+            .call(x_final);
+
+        //Y Axis
+        y_pre = d3.scaleLinear()
+            .domain([0, 100])
+            .range([height, 10]);
+
+        y_final = function(svg){
+            svg.call(d3.axisLeft(y_pre).ticks(3))
+            svg.call(function(g){
+                g.selectAll('.tick line')
+                    .attr('class', function(d,i) {
+                        if (d == 0) {
+                            return 'line-special';
+                        }
+                    })
+                    .attr("x1", '0')
+                    .attr("x2", '' + width + '')
+            })
+            svg.call(function(g){g.select('.domain').remove()});
+        }      
+
+        chart.append("g")
+            .attr('class','y_axis')
+            .call(y_final);
+
+        //Lines
+        line = d3.line()
+            .x(function(d) { return x_pre(d.Year) + x_pre.bandwidth() / 2; })
+            .y(function(d) { return y_pre(+d.value); });
+
+        paths = chart.selectAll(".line")
+            .data(nestedData)
+            .enter()
+            .append("path")
+            .attr('class', 'line')
+            .attr("fill", "none")
+            .attr("stroke", 'red')
+            .attr("stroke-width", 2)
+            .attr('d', function(d) {
+                return line(d.data);
+            });
+
+        paths.attr("stroke-dasharray", 768 + " " + 768)
+            .attr("stroke-dashoffset", 768)
+            .transition()
+            .ease(d3.easeLinear)
+            .attr("stroke-dashoffset", 0)
+            .duration(3000);
+
+        //Quedarían los círculos para el tooltip
     });
 }
 
